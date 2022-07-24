39c6c524c85028207b81ea9bf1dab1f121f206e6
remedydev <107086502+remedyfn@users.noreply.github.com>
2022-07-23 22:04:05 -0700

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,104 @@
+from nltk.chat.util import Chat, reflections
+
+
+
+
+#Pairs is a list of patterns and responses.
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is remedyfn, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*)created(.*)",
+        ["remedyfn created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Sweden, Stockholm',]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+    ],
+    [
+        r"how (.*) health (.*)",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I love Breeding cats!",]
+    ],
+    [
+        r"who (.*) (Breeder|Batsman)?",
+        ["My maker and his name remedy!"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [
+        r"(.*)",
+        ['That is nice to hear']
+    ],
+]
+
+
+print(reflections)
+
+
+#Outputs
+{'i am': 'you are',
+'i was': 'you were',
+'i': 'you',
+"i'm": 'you are',
+"i'd": 'you would',
+"i've": 'you have',
+"i'll": 'you will',
+'my': 'your',
+'you are': 'I am',
+'you were': 'I was',
+"you've": 'I have',
+"you'll": 'I will',
+'your': 'my',
+'yours': 'mine',
+'you': 'me',
+'me': 'you'}
+
+my_dummy_reflections= {
+    "go"     : "gone",
+    "hello"    : "hey there"
+}
+
+print("Hi, I'm remedy and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+chat = Chat(pairs, reflections)
+chat.converse()
